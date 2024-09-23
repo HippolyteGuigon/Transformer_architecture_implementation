corrected layer normalisation calculus with appropriate dimension

transformer_architecture/utils/normalization.py
@@ -34,13 +34,42 @@ class NormalizationLayer:
     ) -> None:
         self.eps = eps
 
-        if elementwise_affine:
+        self.elementwise_affine = elementwise_affine
+        self.bias = bias
+
+        if self.elementwise_affine:
             elementwise_affine_dim = normalized_shape[-1]
             self.gamma = torch.randn(
                 (elementwise_affine_dim), requires_grad=True
             )
 
-            if bias:
+            if self.bias:
                 self.beta = torch.randn(
                     (elementwise_affine_dim), requires_grad=True
                 )
+
+    def forward(self, input: torch.Tensor) -> torch.Tensor:
+        """
+        The goal of this method is to implement the
+        normalization layer process to an input
+        tensor
+
+        Arguments:
+            -input: torch.Tensor: The input Tensor to
+            be normalized
+        Returns:
+            -normalized_layer: torch.Tensor: The input
+            after it was normalized
+        """
+
+        mean = torch.mean(input, dim=-1, keepdim=True)
+        standard_deviation = torch.std(input, dim=-1, keepdim=True)
+
+        normalized_input = (input - mean) / standard_deviation
+
+        if self.elementwise_affine:
+            normalized_input *= self.gamma
+            if self.bias:
+                normalized_input += self.beta
+
+        return normalized_input