df351b44f23b3bd2988c6b084170115d3247dbf3
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2024-09-23 11:12:19 +0200

    added alpha and gamma terms for normalization

--- transformer_architecture/utils/normalization.py
+++ transformer_architecture/utils/normalization.py
@@ -1,3 +1,5 @@
+import torch
+
 from typing import List
 
 
@@ -13,11 +15,26 @@ class NormalizationLayer:
         shape of the element to be normalized
         -eps: Value added to the denominator
         for numerical stability
+        -elementwise_affine: bool: If the normalization
+        should have learnable parameters Beta and Gamma
+        learnt during the process
     Returns:
         -None
     """
 
     def __init__(
-        self, normalized_shape: List[int], eps: float = 1e-05
+        self,
+        normalized_shape: List[int],
+        eps: float = 1e-05,
+        elementwise_affine: bool = True,
     ) -> None:
         self.eps = eps
+
+        if elementwise_affine:
+            elementwise_affine_dim = normalized_shape[-1]
+            self.alpha = torch.randn(
+                (elementwise_affine_dim), requires_grad=True
+            )
+            self.gamma = torch.randn(
+                (elementwise_affine_dim), requires_grad=True
+            )
